9f012fbf1b06ebac080efb52040a276493d31be1
EDUARDO DE SOUZA <eduardo_souza28@estudante.sesisenai.org.br>
2025-02-03 16:13:47 -0300

    exercicio 2

--- atvs.js
+++ atvs.js
@@ -1,12 +1,24 @@
-let num = 2;
+let num = 3;
+let num2 = 5
 
 function parOuImpar(int) {
     if (num % 2 == 0) {
-        console.log("par")
+        console.log(int + " é par" + "\n")
     } else {
-        console.log("impar")
+        console.log(int + " é impar" + "\n")
     }
 }
 
+function numeroMaoir(n1, n2) {
+    if (n1 > n2) {
+        console.log(n1 + " é maior que " + n2 + "\n");
+    } else {
+        console.log(n2 + " é maior que " + n1 + "\n");
+    }
+}
+
+
+numeroMaoir(num,num2)
 parOuImpar(num)
+
 
