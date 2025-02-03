4d145281b116751e013f195d89a9bfa2eabee13a
EDUARDO DE SOUZA <eduardo_souza28@estudante.sesisenai.org.br>
2025-02-03 17:00:10 -0300

    exercicios 3,4,5

--- atvs.js
+++ atvs.js
@@ -1,11 +1,23 @@
-let num = 3;
+let num = 3
 let num2 = 5
+//talvez seja util depois
+// let num3 = -7
+// let num4 = 14
+// let num5 = -3
 
-function parOuImpar(int) {
+
+//em ordem de feitio
+parOuImpar(num)
+numeroMaoir(num,num2)
+somaFatorial(num)
+fatorial(num)
+contarNumeros()
+
+function parOuImpar(n) {
     if (num % 2 == 0) {
-        console.log(int + " é par" + "\n")
+        console.log(n + " é par" + "\n")
     } else {
-        console.log(int + " é impar" + "\n")
+        console.log(n + " é impar" + "\n")
     }
 }
 
@@ -17,8 +29,35 @@ function numeroMaoir(n1, n2) {
     }
 }
 
+function somaFatorial (n) {
+    let soma = 0;
+    for (i = 0; i <= n; i++){
+        soma += i
+    }
+    console.log(soma + "\n")
+}
 
-numeroMaoir(num,num2)
-parOuImpar(num)
+function fatorial (n) {
+    let soma = 1;
+    for (i = 1; i <= n; i++){
+        soma *= i
+    }
+    console.log(soma + "\n")
+}
+
+function contarNumeros(){
+    let pos = 0
+    let neg = 0
+    numeros = [2,5,-5,3,-7]
+    for (i = 0; i < 5; i++){
+        if(numeros[i] < 0){
+            neg ++
+        } else {
+            pos ++
+        }
+    }
+    console.log(pos + " numeros positivos \r" + neg + " numeros negativos\n")
+}
+
 
 
