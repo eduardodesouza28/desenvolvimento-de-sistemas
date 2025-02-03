ca202b0d691451f32b7ee625a01af5eb2bdede74
EDUARDO DE SOUZA <eduardo_souza28@estudante.sesisenai.org.br>
2025-02-03 17:35:52 -0300

    exercicios 6,7,8

--- atvs.js
+++ atvs.js
@@ -1,4 +1,4 @@
-let num = 3
+let num = 4
 let num2 = 5
 //talvez seja util depois
 // let num3 = -7
@@ -7,11 +7,14 @@ let num2 = 5
 
 
 //em ordem de feitio
-parOuImpar(num)
-numeroMaoir(num,num2)
-somaFatorial(num)
-fatorial(num)
-contarNumeros()
+// parOuImpar(num)//1
+// numeroMaoir(num, num2)//2
+// somaFatorial(num)//3
+// fatorial(num)//4
+// contarNumeros()//5
+// operacoes(num, num2)//6
+numerosPrimo(num)//7
+// tabuada(num)//8
 
 function parOuImpar(n) {
     if (num % 2 == 0) {
@@ -29,35 +32,79 @@ function numeroMaoir(n1, n2) {
     }
 }
 
-function somaFatorial (n) {
+function somaFatorial(n) {
     let soma = 0;
-    for (i = 0; i <= n; i++){
+    for (i = 0; i <= n; i++) {
         soma += i
     }
     console.log(soma + "\n")
 }
 
-function fatorial (n) {
+function fatorial(n) {
     let soma = 1;
-    for (i = 1; i <= n; i++){
+    for (i = 1; i <= n; i++) {
         soma *= i
     }
     console.log(soma + "\n")
 }
 
-function contarNumeros(){
+function contarNumeros() {
     let pos = 0
     let neg = 0
-    numeros = [2,5,-5,3,-7]
-    for (i = 0; i < 5; i++){
-        if(numeros[i] < 0){
-            neg ++
+    numeros = [2, 5, -5, 3, -7]
+    for (i = 0; i < 5; i++) {
+        if (numeros[i] < 0) {
+            neg++
         } else {
-            pos ++
+            pos++
         }
     }
     console.log(pos + " numeros positivos \r" + neg + " numeros negativos\n")
 }
 
+function operacoes(n1, n2) {
+    multiplica(n1, n2)
+    soma(n1, n2)
+    subtrai(n1, n2)
+    divide(n1, n2)
+}
+
+function multiplica(n1, n2) {
+    console.log(n1 * n2)
+}
+function soma(n1, n2) {
+    console.log(n1 + n2)
+}
+function subtrai(n1, n2) {
+    console.log(n1 - n2)
+}
+function divide(n1, n2) {
+    console.log(n1 / n2 + "\n")
+}
+
+function numerosPrimo(n){
+    primo = true
+    for (i = 0; i < n ;i++){
+            if (i != 1 && i != n){
+                console.log("aaa")
+                primo = true
+            } else {
+                console.log("aaa")
+                primo = false
+            }
+        }
+    if (primo == true){
+        console.log(n + " é primo")
+    } else {
+        console.log(n + " não é primo")
+    }
+}
+
+function tabuada(n){
+    for(i = 1; i <= 10; i++){
+        console.log(n * i)
+    }
+
+}
 
 
