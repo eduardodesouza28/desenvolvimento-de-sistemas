0c3600f99b03f727e510c8ccd698f894b56867ea
eduardodesouza28 <eduardo_souza28@estudante.sesisenai.org.br>
2025-02-25 15:13:10 -0300

    começo orientação a objetos

--- orientação a objeto/atvs.js
+++ orientação a objeto/atvs.js
@@ -1,15 +1,74 @@
-const veiculo  = {
-    velocidade: 0,
+class Pessoa {
+    constructor(nome, dt_nsc) {
+        this.nome = nome,
+            this.dt_nsc = dt_nsc
+    }
 
-    acelerar: function acelerar() {
+}
+
+const menino = new Pessoa(
+    'luiz',
+    '01/01/2001'
+)
+
+const propCarro = {
+    tipo: 'sedan',
+    ano: 2019,
+    marca: 'fiat',
+    cor: 'prata',
+    velocidade: 10,
+    passageiros: menino.nome
+}
+
+const porpBarco = {
+    ano: 2024,
+    marca: 'pantanaltica',
+    cor: 'azul'
+}
+
+class Veiculo {
+    constructor(propCarro) {
+        this.tipo = propCarro.tipo
+        this.ano = propCarro.ano
+        this.marca = propCarro.marca
+        this.cor = propCarro.cor
+        this.velocidade = propCarro.velocidade
+        this.passageiros = propCarro.passageiros
+    }
+    acelerar = function () {
         this.velocidade += 10
-    },
-    freiar: function freiar(){
-        this.velocidade -= 5
+    }
+    freiar = function () {
+        this.velocidade -= 10
     }
 }
-console.log(veiculo.velocidade)
-veiculo.acelerar()
-veiculo.acelerar()
-veiculo.freiar()
-console.log(veiculo.velocidade)
+
+class Barco extends Veiculo {
+    constructor(propCarro) {
+        super(propCarro),
+        this.tipo = 'barco'
+        this.nós = 0
+    }
+    acelerar = function () {
+        this.nós += 5
+    }
+    freiar = function () {
+        this.nós -= 5
+    }
+}
+
+const carro = new Veiculo(
+    propCarro
+)
+const barco = new Barco(
+    porpBarco,
+
+)
+
+for (i = 0; i < 3; i++) {
+    barco.acelerar()
+}
+barco.freiar()
+console.log(barco)
+carro.acelerar()
+
