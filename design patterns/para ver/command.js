fb8d6488f0043d100cc2748575eca117a6628869
eduardodesouza28 <eduardo_souza28@estudante.sesisenai.org.br>
2025-03-13 16:24:11 -0300

    command

--- design patterns/para ver/command.js
+++ design patterns/para ver/command.js
@@ -1,1 +1,75 @@
 //comportamental
+
+class Command {
+  execute() { }
+}
+class OnCommand extends Command {
+  constructor(lightBulb) {
+    super();
+    this.lightBulb = lightBulb;
+  }
+
+  execute() {
+    this.lightBulb.On();
+  }
+}
+
+class OffCommand extends Command {
+  constructor(lightBulb) {
+    super();
+    this.lightBulb = lightBulb;
+  }
+
+  execute() {
+    this.lightBulb.Off();
+  }
+}
+class MidCommand extends Command {
+  constructor(lightBulb) {
+    super();
+    this.lightBulb = lightBulb;
+  }
+
+  execute() {
+    this.lightBulb.Mid();
+  }
+}
+class LightBulb {
+  On() {
+    console.log('Bulb is ON');
+  }
+
+  Off() {
+    console.log('Bulb is OFF');
+  }
+  Mid() {
+    console.log('Bulb is MID');
+  }
+}
+class Switch {
+  constructor() {
+    this.command = null;
+  }
+
+  setCommand(command) {
+    this.command = command;
+  }
+
+  executeCommand() {
+    this.command.execute();
+  }
+}
+const light = new LightBulb();
+const On = new OnCommand(light);
+const Off = new OffCommand(light);
+const Mid = new MidCommand(light);
+const switchButton = new Switch();
+
+switchButton.setCommand(On);
+switchButton.executeCommand();
+
+switchButton.setCommand(Off);
+switchButton.executeCommand();
+
+switchButton.setCommand(Mid);
+switchButton.executeCommand();
