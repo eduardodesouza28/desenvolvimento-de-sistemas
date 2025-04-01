977bd7748ba3de3ecc71a67e069f15ef22b54ae0
eduardodesouza28 <eduardo_souza28@estudante.sesisenai.org.br>
2025-04-01 17:09:28 -0300

    exportando as funções no index.js

--- novo pacote/index.js
+++ novo pacote/index.js
@@ -2,8 +2,10 @@ const is_not_even = require("./utils/isNotEven")
 const kelvin = require("./utils/kelvin")
 const celsius = require("./utils/celsius")
 const fahrenheit = require("./utils/fahrenheit")
-const milesKm = require("./utils/kmMilha")
-const meterYards = require("./utils/metersYards")
-const centFeet = require("./utils/centFeet")
-const kgLibres = require("./utils/kgLibres")
+const mileskm = require("./utils/kmMilha")
+const meteryards = require("./utils/metersYards")
+const centfeet = require("./utils/centFeet")
+const kglibres = require("./utils/kgLibres")
 
+
+module.exports = {is_not_even,kelvin,celsius,fahrenheit,mileskm,meteryards,centfeet,kglibres}
