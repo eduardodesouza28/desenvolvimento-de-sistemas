fabe00b006defd07d1b1e0e75ce110454ab951ce
eduardodesouza28 <eduardo_souza28@estudante.sesisenai.org.br>
2025-04-15 14:05:14 -0300

    rotas react

---
+++ react/landing-page/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
