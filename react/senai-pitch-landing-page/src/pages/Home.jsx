28f63b90c1ac197ef2dd5d7343df3123071c064b
eduardodesouza28 <eduardo_souza28@estudante.sesisenai.org.br>
2025-04-16 15:49:49 -0300

    site landing-page mudanças

--- react/senai-pitch-landing-page/src/pages/Home.jsx
+++ react/senai-pitch-landing-page/src/pages/Home.jsx
@@ -6,11 +6,11 @@ function Home() {
     <div className="container">
       <section id="home-intro">
         <h1>flappy bird protótipo</h1>
-        <h2>Seu Slogan ou Frase de Impacto Aqui!</h2>
-        <p>Uma breve descrição que convida o usuário a conhecer mais sobre a solução inovadora desenvolvida no Mundo SENAI.</p>
+        <h2>Viciante como o original, desafiador como nunca!</h2>
+        <p>uma cópia do famoso jogo "Flappy Bird" apenas para fins de estudo</p>
 
 
-        <img src="src/assets/flappy.png" alt="Banner do Projeto" className="responsive banner" />
+        {/* <img src="src/assets/flappy.png" alt="Banner do Projeto" className="responsive banner" /> */}
 
         <Link to="/projeto" className="btn">Saiba Mais Sobre o Projeto</Link>
       </section>
